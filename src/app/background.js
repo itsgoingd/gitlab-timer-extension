4a2c151a002b4ec9e7fbd6ede11bb7e76277ede7
its <itsgoingd@luzer.sk>
2018-05-28 13:36:34 +0200

    Added support for timers in merge requests.

--- src/app/background.js
+++ src/app/background.js
@@ -3,7 +3,7 @@ let api = chrome || browser
 api.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if (changeInfo.status != 'complete') return
 
-	if (! tab.title.match(new RegExp('.+? \\(#\\d+\\) · Issues · .+? · GitLab'))) return
+	if (! tab.title.match(new RegExp('.+? \\([#!]\\d+\\) · (Issues|Merge Requests) · .+? · GitLab'))) return
 
 	chrome.tabs.executeScript(tabId, { file: 'app/timer.js' })
 })
